e35f37e70c86468e5713de195286aa18a2cbe702
Craig MacFarlane <camacfarl@gmail.com>
2014-01-28 21:14:27 -0800

    Adds #defines for motor controller and port

--- 2014/test/pidI2CTest.c
+++ 2014/test/pidI2CTest.c
@@ -3,13 +3,14 @@
 
 #include "..\library\ht_motor.h"
 
-
+#define MOTOR_CTRL 0
+#define MOTOR_PORT 0
 task main()
 {
     HT_MotorInit(controller_chain);
-    HT_MotorSetPIDControl(0, 0, true);
-    HT_MotorSetSpeed(0, 0, 10);
-    HT_MotorOn(0, 0);
+    HT_MotorSetPIDControl(MOTOR_CTRL, MOTOR_PORT, true);
+    HT_MotorSetSpeed(MOTOR_CTRL, MOTOR_PORT, 10);
+    HT_MotorOn(MOTOR_CTRL, MOTOR_PORT);
     /*wait1Msec(2000);
     HT_MotorOff(0, 0);
     wait1Msec(2000);
@@ -18,6 +19,6 @@ task main()
     HT_MotorOff(0, 0);*/
 
     while (true) {
-        nxtDisplayCenteredTextLine(3, "%d", HT_MotorEncoderValue(0, 0));
+        nxtDisplayCenteredTextLine(3, "%d", HT_MotorEncoderValue(MOTOR_CTRL, MOTOR_PORT));
     }
 }
