794147318a4a7a5b2988bd5a3daaabc1249bc6fe
megan2001 <megantran@rocknrollrobots25.com>
2014-10-16 16:37:35 -0700

    IR beacon HW

--- RNRR6_00/IR_beaconn.c
+++ RNRR6_00/IR_beaconn.c
@@ -1,13 +1,53 @@
-task main()
-{
-typeDef enum dir_{
-	DIR_right,
-	DIR_left,
-	DIR_center,
-} Direction;
 
-direction get_DIR_to_beacon(void)
+typedef enum dir_{
+	right,
+	left,
+	center,
+} direction;
+
+direction get_dir_to_beacon (void)
 {
 	int segment;
-	segment=HTIRS2readACDir(irRECIVER);
+	segment = HTIRS2readACDir(irRECIVER);
+
+	switch (segment) {
+
+	case 1:
+	case 2:
+	case 3:
+	case 4:
+		return left;
+		break;
+
+	case 5:
+		return center;
+		break;
+
+	case 6:
+	case 7:
+	case 8:
+	case 9:
+		return right;
+	 	break;
+	}
+}
+
+task main()
+{
+	while (true) {
+ 	direction get_dir_to_beacon;
+
+	switch
+
+		case left:
+		return nxtDisplayTextLine ( 5 , "turning left" )
+		break;
+
+		case right:
+		return nxtDisplayTextLine ( 5 , "turning right" )
+		break;
+
+		case center:
+		return nxtDisplayTextLine ( 5 , "moving foward" )
+	}
 }
