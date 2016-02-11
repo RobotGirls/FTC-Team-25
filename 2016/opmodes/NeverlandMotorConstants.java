0d8ab5cf01ddebb2319f7313f19d304444f0fc14
izzielau <izzielau@rocknrollrobots25.com>
2016-02-10 20:01:23 -0800

    Neverland constants.

---
+++ 2016/opmodes/NeverlandMotorConstants.java
@@ -0,0 +1,13 @@
+package opmodes;
+
+import team25core.Robot;
+
+/**
+ * Created by Izzie on 2/6/2016.
+ */
+public class NeverlandMotorConstants {
+    public static final int ENCODER_TICKS_PER_INCH = 212;
+    public static final int ENCODER_TICKS_PER_DEGREE = 26;
+
+    public static final double ADJUST_PID = 0.0251;
+}
--- 2016/opmodes/NeverlandServoConstants.java
+++ 2016/opmodes/NeverlandServoConstants.java
@@ -1,7 +1,7 @@
 package opmodes;
 
 /*
- * FTC Team 25: Izzie_Express, December 05, 2015
+ * FTC Team 25: izzielau, December 05, 2015
  */
 
 public class NeverlandServoConstants {
@@ -12,8 +12,16 @@ public class NeverlandServoConstants {
     public final static float LEFT_FLAG_STOWED      = (float)185.0/(float)256;
     public final static float LEFT_FLAG_NINETY      = (float)126.0/(float)256;
 
-    public final static float LEFT_PUSHER_DEPLOYED  = (float)256.0/(float)256;
+    public final static float RIGHT_BUMPER_UP      = (float)231/(float)256;
+    public final static float RIGHT_BUMPER_DOWN    = (float)012/(float)256;
+    public final static float LEFT_BUMPER_UP       = (float)031/(float)256;
+    public final static float LEFT_BUMPER_DOWN     = (float)251/(float)256;
+
+    public final static float LEFT_PUSHER_DEPLOYED  = (float)256/(float)256;
     public final static float LEFT_PUSHER_STOWED    = (float)0.0/(float)256;
     public final static float RIGHT_PUSHER_DEPLOYED = (float)0.0/(float)256;
-    public final static float RIGHT_PUSHER_STOWED   = (float)256.0/(float)256;
+    public final static float RIGHT_PUSHER_STOWED   = (float)256/(float)256;
+
+    public final static float CLIMBER_SCORE = (float)225.0/(float)256.0;
+    public final static float CLIMBER_STORE = (float)0.0/(float)256.0;
 }
