e7e24b6d7e865877606e19bc10e533922eec3437
Craig MacFarlane <camacfarl@gmail.com>
2016-02-24 20:14:09 -0800

    Autonomous constants.

---
+++ 2016/opmodes/NeverlandAutonomousConstants.java
@@ -0,0 +1,10 @@
+package opmodes;/*
+ * FTC Team 25: cmacfarl, February 24, 2016
+ */
+
+public class NeverlandAutonomousConstants {
+    public static final int DISTANCE_FROM_BEACON = 19;
+    public static final int TURN_SAFETY_TIME = 1500;
+    public static final int ULTRASONIC_DIFFERENCE = 0;
+    public static final int MOVING_AVG_SET_SIZE = 6;
+}
