7a5bc30f6712554d8f3de228b7e46318588deb1b
izzielau <izzielau@rocknrollrobots25.com>
2016-03-08 19:39:46 -0800

    Adds light polarity as a parameter.
    Allows the light sensor to look for either black or white.

--- java/team25core/LightSensorCriteria.java
+++ java/team25core/LightSensorCriteria.java
@@ -5,32 +5,62 @@ package team25core;
  */
 
 import com.qualcomm.robotcore.hardware.LightSensor;
+import com.qualcomm.robotcore.util.RobotLog;
 
 /*
  * Looks for a white line.
  */
 public class LightSensorCriteria implements SensorCriteria {
 
+    public enum LightPolarity {
+        BLACK,
+        WHITE,
+    }
+
+    protected int light;
+
     protected int min;
     protected int max;
     protected int threshold;
     protected LightSensor sensor;
+    protected LightPolarity polarity;
 
     public LightSensorCriteria(LightSensor sensor, int min, int max)
     {
         this.sensor = sensor;
+        this.polarity = LightPolarity.WHITE;
         this.min = min;
         this.max = max;
         this.threshold = (min + ((max - min)/2));
     }
 
+    public LightSensorCriteria(LightSensor sensor, LightPolarity polarity, int min, int max)
+    {
+        this.sensor = sensor;
+        this.polarity = polarity;
+        this.min = min;
+        this.max = max;
+        this.threshold = (min + ((max - min)/2));
+    }
+
+    public void setThreshold(double percent) {
+        this.threshold = (int)(max - ((max - min) * percent));
+    }
+
     @Override
     public boolean satisfied()
     {
-        if (sensor.getLightDetectedRaw() < threshold) {
-            return true;
-        } else {
-            return false;
+        RobotLog.i("251 Light: %d, Threshold: %d", sensor.getLightDetectedRaw(), threshold);
+        if (polarity == LightPolarity.WHITE) {
+            if (sensor.getLightDetectedRaw() < threshold) {
+                return true;
+            }
+        } else if (polarity == LightPolarity.BLACK) {
+            if (sensor.getLightDetectedRaw() > threshold) {
+                return true;
+            }
         }
+        return false;
     }
 }
+
