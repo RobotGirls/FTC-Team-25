b636e65e3e4d4d19ee240c04c310e6942702045c
izzielau <izzielau@rocknrollrobots25.com>
2016-03-14 21:27:28 -0700

    Added Robot availability to constructor.
    Allows events to be based off of a robot instead of a task.

--- java/team25core/RobotEvent.java
+++ java/team25core/RobotEvent.java
@@ -9,20 +9,41 @@ public class RobotEvent
      * The task this event is associated with
      */
     protected RobotTask task;
+    protected Robot robot;
+
+    /*
+     * For events associated with tasks.
+     */
 
     public RobotEvent(RobotTask task)
     {
         this.task = task;
+        this.robot = null;
     }
 
     /*
-     * Call the event handler for the particular task.  The
+     * For events detached from a task.
+     */
+    public RobotEvent(Robot robot)
+    {
+        this.robot = robot;
+        this.task = null;
+    }
+
+    /*
+     * Call the event handler for the particular task/robot.  The
      * default event handler simply hands the task off the to
-     * robot.
+     * robot. Events should not be associated with a task and a
+     * robot at the same time.
      */
     public void handleEvent()
     {
         task.handleEvent(this);
+        if (task != null) {
+            task.handleEvent(this);
+        } else if (robot != null) {
+            robot.handleEvent(this);
+        }
     }
 
     public String toString()
