b05bae1ad51257df2e9f4bc64e381c2464175df8
Craig MacFarlane <camacfarl@gmail.com>
2016-04-09 15:23:43 -0700

    Adds a linear opmode gamepad test.

---
+++ 2016/test/GamepadLinearTest.java
@@ -0,0 +1,47 @@
+package test;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.swerverobotics.library.interfaces.Autonomous;
+
+@Autonomous(name = "GamepadTest")
+public class GamepadLinearTest extends LinearOpMode{
+
+    public enum Alliance {
+        RED,
+        BLUE,
+        PURPLE,
+    };
+
+    Alliance alliance;
+
+    private boolean done;
+
+    protected Alliance selectAlliance() throws InterruptedException
+    {
+        Alliance alliance = Alliance.PURPLE;
+
+        while (!done) {
+            if (gamepad1.a) {
+                alliance = Alliance.RED;
+            }
+            if (gamepad1.b) {
+                alliance = Alliance.BLUE;
+            }
+            waitOneFullHardwareCycle();
+            telemetry.addData("Alliance: ", alliance.toString());
+        }
+
+        return alliance;
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        done = false;
+        alliance = selectAlliance();
+
+        waitForStart();
+
+        telemetry.addData("Alliance: ", alliance.toString());
+    }
+}
