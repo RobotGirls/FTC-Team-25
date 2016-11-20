97cf13bf4c444bbcef1c83707a6016094114a7d7
Katelyn Biesiadecki <biesiadecki.katelyn.141@gmail.com>
2016-11-19 22:10:53 -0800

    Creation of encoder calibration tests for Daisy.

---
+++ 2017/test/DaisyTurnTest.java
@@ -0,0 +1,100 @@
+/* Copyright (c) 2015 Craig MacFarlane
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of Craig MacFarlane nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+package test;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import opmodes.DaisyConfiguration;
+import team25core.DeadReckon;
+import team25core.DeadReckonTask;
+import team25core.FourWheelGearedDriveDeadReckon;
+import team25core.Robot;
+import team25core.RobotEvent;
+
+@Autonomous(name="Daisy: Turn Test", group="AutoTeam25")
+public class DaisyTurnTest extends Robot {
+
+    private DcMotor frontRight;
+    private DcMotor frontLeft;
+    private DcMotor rearLeft;
+    private DcMotor rearRight;
+    private DeadReckonTask deadReckonTask;
+    private final static double STRAIGHT_SPEED = DaisyConfiguration.STRAIGHT_SPEED;
+    private final static double TURN_SPEED = DaisyConfiguration.TURN_SPEED;
+    private final static int TICKS_PER_INCH = DaisyConfiguration.TICKS_PER_INCH;
+    private final static int TICKS_PER_DEGREE = DaisyConfiguration.TICKS_PER_DEGREE;
+    private FourWheelGearedDriveDeadReckon deadReckon;
+
+    @Override
+    public void handleEvent(RobotEvent e)
+    {
+        // Nothing.
+    }
+
+    @Override
+    public void init()
+    {
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        rearLeft = hardwareMap.dcMotor.get("rearLeft");
+        rearRight = hardwareMap.dcMotor.get("rearRight");
+
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rearLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rearRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rearLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rearRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        deadReckon = new FourWheelGearedDriveDeadReckon(this, TICKS_PER_INCH, TICKS_PER_DEGREE, frontLeft, frontRight, rearLeft, rearRight);
+        deadReckon.addSegment(DeadReckon.SegmentType.TURN, 90, TURN_SPEED);
+    }
+
+    @Override
+    public void start()
+    {
+        deadReckonTask = new DeadReckonTask(this, deadReckon);
+        addTask(deadReckonTask);
+
+    }
+
+    public void stop()
+    {
+        if (deadReckonTask != null) {
+            deadReckonTask.stop();
+        }
+    }
+}
