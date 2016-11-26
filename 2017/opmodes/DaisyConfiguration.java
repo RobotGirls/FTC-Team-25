ec0077484cc6ca7ce9d0862997a659a29e7e3dc7
elixawu <elixawu@gmail.com>
2016-11-25 20:25:19 -0800

    Changed encoder ticks for Daisy

--- 2017/opmodes/DaisyConfiguration.java
+++ 2017/opmodes/DaisyConfiguration.java
@@ -7,12 +7,12 @@ package opmodes;
 public class DaisyConfiguration
 {
     // Autonomous constants.
-    public static final int TICKS_PER_INCH = 100;
-    public static final int TICKS_PER_DEGREE = 100;
+    public static final int TICKS_PER_INCH = 60;
+    public static final int TICKS_PER_DEGREE = 14;
     public final static double STRAIGHT_SPEED = 0.8;
     public final static double TURN_SPEED = 0.4;
-    public final static double ODS_MIN = 1.3;
-    public final static double ODS_MAX = 4.5;
+    public final static double ODS_MIN = 1;
+    public final static double ODS_MAX = 5;
 
     // Teleop constants. Maybe I'll use these, maybe not...
     public final static double FLOWER_POWER = 1.0;
