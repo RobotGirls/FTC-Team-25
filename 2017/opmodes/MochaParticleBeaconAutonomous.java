f2e1e94cf6ab3cadb4188728340d2b16cb8c8c38
Lizzie Lau <Lizzie Lau>
2016-12-10 00:32:22 -0800

    Renaming files, autonomous changes, configuration file added for continuity between different programs.

--- 2017/opmodes/MochaParticleBeaconAutonomous.java
+++ 2017/opmodes/MochaParticleBeaconAutonomous.java
@@ -3,8 +3,6 @@ package opmodes;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
-import com.qualcomm.robotcore.hardware.HardwareDevice;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.RobotLog;
 
 import team25core.DeadReckon;
@@ -15,16 +13,12 @@ import team25core.Robot;
 import team25core.RobotEvent;
 import team25core.RunToEncoderValueTask;
 import team25core.SingleShotTimerTask;
-import team25core.SingleWheelDirectDriveDeadReckon;
-import team25core.Team25DcMotor;
 
 /**
  * Created by Lizzie on 11/19/2016.
  */
-@Autonomous(name = "Particle Cap Ball Autonomous", group = "AutoTest")
-public class ParticleCapBallAutonomous extends Robot {
-    private static final int TICKS_PER_DEGREE = 22;
-    private static final int TICKS_PER_INCH = 79;
+@Autonomous(name = "Mocha Particle Beacon Autonomous", group = "AutoTest")
+public class MochaParticleBeaconAutonomous extends Robot {
 
     private DcMotorController mc;
     private DcMotor frontLeft;
@@ -36,10 +30,11 @@ public class ParticleCapBallAutonomous extends Robot {
     private DcMotor sbod;
     private int paddleCount;
 
+    private FourWheelDirectDriveDeadReckon positionBeacon;
+    private DeadReckonTask positionBeaconTask;
     private RunToEncoderValueTask scoreCenterDeadReckonTask;
     private PeriodicTimerTask ptt;
-    private FourWheelDirectDriveDeadReckon pushCapDeadReckon;
-    private DeadReckonTask pushCapDeadReckonTask;
+
 
     @Override
     public void handleEvent(RobotEvent e)
@@ -47,13 +42,15 @@ public class ParticleCapBallAutonomous extends Robot {
         if (paddleCount >= 10) {
             ptt.stop();
             stopShooter();
-            addTask(pushCapDeadReckonTask);
+            addTask(positionBeaconTask);
         }
+
         if (e instanceof PeriodicTimerTask.PeriodicTimerEvent) {
-            RobotLog.i("163: Period timer task expired, %d", paddleCount);
+            RobotLog.i("163 Period timer task expired, %d", paddleCount);
             paddleCount++;
             addTask(scoreCenterDeadReckonTask);
         }
+
     }
 
     @Override
@@ -77,26 +74,29 @@ public class ParticleCapBallAutonomous extends Robot {
         frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        scoreCenterDeadReckonTask = new RunToEncoderValueTask(this, sbod, 1, .7);
-        ptt = new PeriodicTimerTask(this, 350);
+        scoreCenterDeadReckonTask = new RunToEncoderValueTask(this, sbod, 1, .8);
+        ptt = new PeriodicTimerTask(this, 300);
 
-        pushCapDeadReckon = new FourWheelDirectDriveDeadReckon
-                (this, TICKS_PER_INCH, TICKS_PER_DEGREE, frontRight, backRight, frontLeft, backLeft);
-        pushCapDeadReckon.addSegment(DeadReckon.SegmentType.STRAIGHT, 68, -.75);
-        pushCapDeadReckonTask = new DeadReckonTask(this, pushCapDeadReckon);
+        positionBeacon = new FourWheelDirectDriveDeadReckon
+                (this, MochaCalibration.TICKS_PER_INCH, MochaCalibration.TICKS_PER_DEGREE, frontRight, backRight, frontLeft, backLeft);
+        positionBeacon.addSegment(DeadReckon.SegmentType.STRAIGHT, 6, -.75);
+        positionBeacon.addSegment(DeadReckon.SegmentType.TURN, 45, -.3);
+        positionBeacon.addSegment(DeadReckon.SegmentType.STRAIGHT, 47, -.75);
+        positionBeacon.addSegment(DeadReckon.SegmentType.TURN, 45, .3);
+        positionBeaconTask = new DeadReckonTask(this, positionBeacon);
+
     }
 
     protected void startShooter()
-
     {
-        shooterLeft.setPower(.575);
-        shooterRight.setPower(-.575);
+        shooterLeft.setPower(.3);
+        shooterRight.setPower(-.3);
     }
 
     protected void stopShooter()
@@ -116,9 +116,5 @@ public class ParticleCapBallAutonomous extends Robot {
                 robot.addTask(scoreCenterDeadReckonTask);
             }
         });
-
-
     }
-
-
 }
