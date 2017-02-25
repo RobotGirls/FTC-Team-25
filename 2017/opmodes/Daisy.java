66b8a111645e1e79c3b75f34a1f2ffaacac0646a
Katelyn Biesiadecki <biesiadecki.katelyn.141@gmail.com>
2017-02-25 12:11:06 -0800

    Added new constants for the standard button pushers.

--- 2017/opmodes/Daisy.java
+++ 2017/opmodes/Daisy.java
@@ -1,5 +1,12 @@
 package opmodes;
 
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+
 /**
  * FTC Team 25: Created by Katelyn Biesiadecki on 11/12/2016.
  */
@@ -18,11 +25,23 @@ public class Daisy
     public final static int COLOR_PORT = 0;
     public final static int COLOR_THRESHOLD = 278;
 
+    // Vuforia constants.
+    public final static VuforiaLocalizer.CameraDirection CAMERA_CHOICE = VuforiaLocalizer.CameraDirection.FRONT;
+    public final static String KEY = "Afbu2Uv/////AAAAGVouNdSAD0P8la+sq37vCdQ6uLVH8NWrBLnfZ1R5rObJQpVVHJzqvIgMZO5gTqXG6DYJZcgwtSVZXU2g20FAJobxCog9Wc5vtqgJJmrsJ0NOABRbi9vy4Y9IzBVfaDoRsQTmjxxFf62Z9slttsb44KopGpVGTQ83iHnTo/wDvnZBWRhmckG6IKuqkbRYCFD+w1hHvVLuDoIYLgfpa1Rw1Pc7rszP/CDzUfeO9KwodFpEsfZHIZI8KHIYzfRIOhg1Tg0T4eRsLCO8s9vfZd6vfTuUA/sZkID3N7BsrlLaL6vUqheGPvsbPuQQsMqgPNYTqbhvv3KI/SR5WxUaccuVHnpVMhAjkdpruWVliCCZqp1t";
+    public final static int CAMERA_FORWARD_DISPLACEMENT  = 0;     // Camera is 110 mm in front of robot center
+    public final static int CAMERA_VERTICAL_DISPLACEMENT = 127;   // Camera is 200 mm above ground
+    public final static int CAMERA_LEFT_DISPLACEMENT     = 64;    // Camera is ON the robots center line
+    public final static OpenGLMatrix PHONE_LOCATION_ON_ROBOT = OpenGLMatrix
+            .translation(CAMERA_FORWARD_DISPLACEMENT, CAMERA_LEFT_DISPLACEMENT, CAMERA_VERTICAL_DISPLACEMENT)
+            .multiplied(Orientation.getRotationMatrix(
+                    AxesReference.EXTRINSIC, AxesOrder.YZX,
+                    AngleUnit.DEGREES, CAMERA_CHOICE == VuforiaLocalizer.CameraDirection.FRONT ? 90 : -90, 0, 0));
+
     // These no longer have any use...
     public final static double LEFT_DEPLOY_POS = 1;
-    public final static double RIGHT_DEPLOY_POS = 0;
-    public final static double LEFT_STOW_POS = 0;
-    public final static double RIGHT_STOW_POS = 1;
+    public final static double RIGHT_DEPLOY_POS = 1;
+    public final static double LEFT_STOW_POS = 0.4;
+    public final static double RIGHT_STOW_POS = 0.3;
 
     // Teleop constants. Maybe I'll use these, maybe not...
     public final static double FLOWER_POWER = 1.0;
