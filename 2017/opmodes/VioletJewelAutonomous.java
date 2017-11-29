e01c5a498e3365e2d55728c4d87dd0fcecbf6a01
Breanna Chan <breannachan@rocknrollrobots25.com>
2017-11-28 20:20:10 -0800

    Made changes to Jewel Autonomous. Problem with arm not going up after first turn still persists. Craig, Breanna, Cindy 11/28/2017.

--- 2017/opmodes/VioletJewelAutonomous.java
+++ 2017/opmodes/VioletJewelAutonomous.java
@@ -115,6 +115,7 @@ public class VioletJewelAutonomous extends Robot {
        // pushJewel   = new DeadReckonPath();
         park        = new DeadReckonPath();
 
+        // Arm initialized up
         jewel.setPosition(0.56);    // 145/256
 
         // Single shot timer tasks for delays.
@@ -134,6 +135,7 @@ public class VioletJewelAutonomous extends Robot {
     @Override
     public void start()
     {
+        // Arm goes down
         jewel.setPosition(0.05);
         // 15/256
         RobotLog.i("506 Arm deployed in start.");
@@ -145,16 +147,21 @@ public class VioletJewelAutonomous extends Robot {
                             @Override
                             public void handleEvent(RobotEvent e) {
                                 DeadReckonEvent path = (DeadReckonEvent) e;
-                                if (path.kind == EventKind.PATH_DONE) {
-                                    jewel.setPosition(0.56);
-                                    RobotLog.i("506 Arm reset to initial position after path done.");
-                                   addTask(new DeadReckonTask(robot, park, drivetrain) {
-                                        @Override
-                                        public void handleEvent(RobotEvent e) {
-                                        }
-                                    });
-
-
+                                switch (path.kind) {
+                                    case SEGMENT_DONE:
+                                        jewel.setPosition(0.56);
+                                        RobotLog.i("506 Arm reset to initial position after segment done.");
+                                        break;
+                                    case PATH_DONE:
+                                        RobotLog.i("506 Arm reset to initial position after path done.");
+                                        addTask(new DeadReckonTask(robot, park, drivetrain) {
+                                            @Override
+                                            public void handleEvent(RobotEvent e) {
+                                            }
+                                        });
+                                        break;
+                                    default:
+                                        break;
                                 }
                             }
                         });
@@ -265,7 +272,7 @@ public class VioletJewelAutonomous extends Robot {
         };
 
         addTask(colorThiefTask);
-        addTask(stt);
+        addTask(stt); //Craig says this doesn't do anything, returns immediately
 
         setupParkPath();
     }
