a6f397edc96d49a72b6d0c873e3e29b5db951f99
Breanna Chan <breannachan@rocknrollrobots25.com>
2018-02-17 11:59:36 -0800

    Program to test reed switch.

---
+++ 2017/test/ReedSwitchTest.java
@@ -0,0 +1,40 @@
+package test;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+/**
+ * Created by Breanna Chan on 2/15/2018.
+ */
+
+@TeleOp(name = "Reed Switch Test", group = "Test")
+@Disabled
+
+public class ReedSwitchTest extends OpMode {
+
+    private Telemetry.Item reedTelemetry;
+    private DigitalChannel reedSwitch;
+    private final static int LIMIT_SWITCH_PORT = 3;;
+
+    @Override
+    public void init()
+    {
+        reedSwitch = hardwareMap.digitalChannel.get("limit");
+
+        reedTelemetry = telemetry.addData("State: ", "Unknown");
+    }
+
+    @Override
+    public void loop()
+    {
+        if (reedSwitch.getState() == true) {        // when opened, reed switch is off magnet
+            reedTelemetry.setValue("Opened");
+        } else {                                    // when closed, reed switch is on magnet
+            reedTelemetry.setValue("Closed");
+        }
+    }
+}
