6d63d7a20415240ce992d5054141c8bc9215ead4
amorygaylord <amorygaylord@rocknrollrobots25.com>
2021-02-12 16:40:39 -0800

    added ring elevator code

--- 2021/opmodes/UltimateGoalTeleop.java
+++ 2021/opmodes/UltimateGoalTeleop.java
@@ -43,8 +43,11 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 import team25core.GamepadTask;
 import team25core.RobotEvent;
 import team25core.SingleGamepadControlScheme;
+import team25core.SingleShotTimerTask;
 import team25core.StandardFourMotorRobot;
 import team25core.TeleopDriveTask;
+
+//TO DO: TEST LAUNCHING & RING ELEVATOR 
 
 @TeleOp(name = "UltimateGoalTeleop")
 //@Disabled
@@ -54,17 +57,22 @@ public class UltimateGoalTeleop extends StandardFourMotorRobot {
     private Telemetry.Item linearEncoderVal;
 
     private TeleopDriveTask drivetask;
-    private DcMotor launchMech;
-    private DcMotor intakeMechLeft;
-    private DcMotor intakeMechRight;
+    private DcMotor launchMechLeft;
+    private DcMotor launchMechRight;
 
     private DcMotor wobbleLift;
     private Servo wobbleGrab;
     private boolean wobbleGrabIsOpen = true;
 
+    private DcMotor ringLift; //hd hex 40
+    private Servo ringDispenser; //continuous servo
+    private boolean ringDispenserExtended = false;
+
     private static final int TICKS_PER_INCH = 79;
     private final double OPEN_WOBBLE_SERVO = (float) 244.0 / 256.0;
     private final double CLOSE_WOBBLE_SERVO = (float) 140.0 / 256.0;
+    private final double DISPENSE_RING = (float) 245.0 / 256.0;
+    private final double RETURN_RING_DISPENSER = (float) 140.0 / 256.0;
 
     //private FourWheelDirectDrivetrain drivetrain;
     //private MechanumGearedDrivetrain drivetrain;
@@ -88,22 +96,25 @@ public class UltimateGoalTeleop extends StandardFourMotorRobot {
         wobbleGrab = hardwareMap.servo.get("wobbleGrabServo");
 
         //mapping the launch mech and intake mech
-        launchMech = hardwareMap.get(DcMotor.class, "launchMech");
-        intakeMechLeft = hardwareMap.get(DcMotor.class, "intakeMechLeft");
-        intakeMechRight = hardwareMap.get(DcMotor.class, "intakeMechRight");
+        launchMechLeft = hardwareMap.get(DcMotor.class, "intakeMechLeft");
+        launchMechRight = hardwareMap.get(DcMotor.class, "intakeMechRight");
 
         //mapping wobble lift motor
         wobbleLift = hardwareMap.get(DcMotor.class, "wobbleLift");
+
+        //mapping the ring elevator motor
+        ringLift = hardwareMap.get(DcMotor.class, "ringLift");
+        ringDispenser = hardwareMap.servo.get("ringDispenser");
 
         // using encoders to record ticks
         backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        launchMech.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        intakeMechLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        intakeMechRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        launchMechLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        launchMechRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         wobbleLift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        ringLift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
        /* launch = new OneWheelDirectDrivetrain(launchMech);
         launch.resetEncoders();
@@ -135,29 +146,39 @@ public class UltimateGoalTeleop extends StandardFourMotorRobot {
                 GamepadEvent gamepadEvent = (GamepadEvent) e;
 
                 switch (gamepadEvent.kind) {
+                    //launching system
                     case BUTTON_X_DOWN:
-                        // enable the launch mech
-                        launchMech.setPower(1);
-                        break;
-                    case BUTTON_X_UP:
-                        // stop the launch mech
-                        launchMech.setPower(0);
+                        //shooting Servo open or closed depending on boolean toggle
+                        if (ringDispenserExtended) {
+                            ringDispenser.setPosition(RETURN_RING_DISPENSER);
+                            ringDispenserExtended = false;
+                        } else {
+                            ringDispenser.setPosition(DISPENSE_RING);
+                            ringDispenserExtended = true;
+                        }
                         break;
                     case BUTTON_Y_DOWN:
-                        //enable the intake mech
-                        intakeMechLeft.setPower(-1); //lines 151+152 and 155+156 might need power vals to be switched (or reduced)
-                        intakeMechRight.setPower(1);
-                    case BUTTON_A_DOWN:
-                        //enable the outtake mech
-                        intakeMechLeft.setPower(1);
-                        intakeMechRight.setPower(-1);
-                        break;
+                        //activate launching mech
+                        launchMechLeft.setPower(0.5);
+                        launchMechRight.setPower(-0.15);
                     case BUTTON_Y_UP:
-                    case BUTTON_A_UP:
-                        // stop the intake/outtake mech
-                        intakeMechLeft.setPower(0);
-                        intakeMechRight.setPower(0);
+                        // stop the launching mech
+                        launchMechLeft.setPower(0);
+                        launchMechRight.setPower(0);
                         break;
+                    case LEFT_TRIGGER_DOWN:
+                        //lift ring elevator UP
+                        ringLift.setPower(1);
+                        break;
+                    case RIGHT_TRIGGER_DOWN:
+                        //ring elevator DOWN
+                        ringLift.setPower(-1); //might have to switch 190 and 194 during testing
+                        break;
+                    case LEFT_TRIGGER_UP:
+                    case RIGHT_TRIGGER_UP:
+                        ringLift.setPower(0);
+                        break;
+                    //wobble goal system
                     case BUTTON_B_DOWN:
                         //wobble servo close OR open depending on boolean toggle;
                         if (wobbleGrabIsOpen) {
@@ -170,16 +191,31 @@ public class UltimateGoalTeleop extends StandardFourMotorRobot {
                         break;
                     case DPAD_UP_DOWN:
                         //wobble lift up
-                        wobbleLift.setPower(0.5); //might have to be changed based on testing
+                        wobbleLift.setPower(0.5);
                         break;
                     case DPAD_DOWN_DOWN:
                         //wobble lift down
-                        wobbleLift.setPower(-0.5); //might have to be changed based on testing
+                        wobbleLift.setPower(-0.5);
                         break;
                     case DPAD_UP_UP:
                     case DPAD_DOWN_UP:
                         wobbleLift.setPower(0);
                         break;
+
+                    /*
+                    BUTTON A: reserved for intake
+                    BUTTON B: wobble grabbing servo
+                    BUTTON X: dispense ring OR retract dispenser
+                    BUTTON Y: activate launching motors
+
+                    L TRIGGER: ring elevator rises
+                    R TRIGGER: ring elevator descends
+
+                    DPAD UP: raise wobble goal
+                    DPAD DOWN: lower wobble goal
+
+                    EMPTY BUTTONS: L DPAD, R DPAD, L BUMPER, R BUMPER
+                     */
                 }
             }
         });
