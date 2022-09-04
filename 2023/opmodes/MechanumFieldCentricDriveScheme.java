e080f3713d2041370398a239d6c5018fe1ae8e0d
ruchi <ruchibondre@rocknrollrobots25.com>
2022-09-03 18:34:46 -0700

    made field centric control scheme

---
+++ 2023/opmodes/MechanumFieldCentricDriveScheme.java
@@ -0,0 +1,86 @@
+package team25core;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+/**
+ * Created by Ruchi Bondre on 9/3/22.
+ */
+
+
+public class MechanumFieldCentricDriveScheme implements JoystickDriveControlScheme {
+
+    /*
+     * An Andymark 40 native spin direction is counterclockwise.
+     */
+    public enum DriveType {
+        DRIVE_GEARED,
+        DRIVE_DIRECT,
+    };
+
+    public enum MotorPosition {
+        OUTER_OPPOSED,
+        INNER_OPPOSED,
+    };
+
+    public enum MotorDirection {
+        CANONICAL,
+        NONCANONICAL,
+    };
+
+
+    protected double rx;
+    protected double x;
+    protected double y;
+
+    protected Gamepad gamepad;
+    protected MotorDirection motorDirection;
+
+    public TankMechanumControlScheme(Gamepad gamepad)
+    {
+        this.gamepad = gamepad;
+        this.motorDirection = MotorDirection.CANONICAL;
+    }
+
+    public TankMechanumControlScheme(Gamepad gamepad, MotorDirection motorDirection)
+    {
+        this.gamepad = gamepad;
+        this.motorDirection = motorDirection;
+    }
+
+    public MotorValues getMotorPowers()
+    {
+        y = -gamepad1.left_stick_y; // Remember, this is reversed!
+        x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+        rx = gamepad1.right_stick_x;
+
+        // If joysticks are pointed left (negative joystick values), counter rotate wheels.
+        // Threshold for joystick values in the x may vary.
+
+        // Read inverse IMU heading, as the IMU heading is CW positive
+        double botHeading = -imu.getAngularOrientation().firstAngle;
+
+        double rotX = x * Math.cos(botHeading) - y * Math.sin(botHeading);
+        double rotY = x * Math.sin(botHeading) + y * Math.cos(botHeading);
+
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio, but only when
+        // at least one is out of the range [-1, 1]
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPower = (rotY + rotX + rx) / denominator;
+        double backLeftPower = (rotY - rotX + rx) / denominator;
+        double frontRightPower = (rotY - rotX - rx) / denominator;
+        double backRightPower = (rotY + rotX - rx) / denominator;
+
+
+
+        return new MotorValues(frontLeftPower, frontRightPower, backLeftPower, backRightPower);
+
+
+
+
+
+
+    }
+}
