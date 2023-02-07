5d31824fdfca17d12ac890359442cc7685cfb528
hAudreyLim <audreylim@rocknrollrobots25.com>
2023-02-06 17:04:15 -0800

    changed encoder values of linearLiftTaskStack

--- 2023/opmodes/ONLYSTACK2.java
+++ 2023/opmodes/ONLYSTACK2.java
@@ -87,6 +87,7 @@ public class ONLYSTACK2 extends Robot {
 
     private DeadReckonPath liftMech;
     private DeadReckonPath  lowerMech;
+    private DeadReckonPath  lowerMech2;
 
 
     private DeadReckonPath strafeOutPath;
@@ -153,6 +154,8 @@ public class ONLYSTACK2 extends Robot {
         lowerMech =  new DeadReckonPath();
         lowerMech.addSegment(DeadReckonPath.SegmentType.STRAIGHT, 10, 0.5);
 
+        lowerMech2 =  new DeadReckonPath();
+        lowerMech2.addSegment(DeadReckonPath.SegmentType.STRAIGHT, 10, 0.5);
         deliverConePath  = new DeadReckonPath();
         deliverConePath.addSegment(DeadReckonPath.SegmentType.SIDEWAYS, 5.5,  -DRIVE_SPEED);
 
@@ -171,8 +174,7 @@ public class ONLYSTACK2 extends Robot {
 
         linearLiftTaskJunction = new RunToEncoderValueTask(this,linearLift,3100,-0.5);
 
-        linearLiftTaskStack = new RunToEncoderValueTask(this,linearLift,3500,0.5);
-
+        linearLiftTaskStack = new RunToEncoderValueTask(this,linearLift,3280,0.5);
 
         strafeOutPath.addSegment(DeadReckonPath.SegmentType.SIDEWAYS,2,0.25);
         strafeOutPath.addSegment(DeadReckonPath.SegmentType.STRAIGHT, 23, -0.25);
@@ -425,20 +427,37 @@ public class ONLYSTACK2 extends Robot {
                 {
                     RobotLog.i("went to middle target zone");
                     whereAmI.setValue("went to middle target zone");
-                    golowerMech2();
+                    delayAndDrop4(2000);
+
 
                 }
             }
         });
     }
 
+    private void delayAndDrop4(int delayInMsec) {
+        this.addTask(new SingleShotTimerTask(this, delayInMsec) {
+            @Override
+            public void handleEvent(RobotEvent e) {
+                SingleShotTimerEvent event = (SingleShotTimerEvent) e;
+                if (event.kind == EventKind.EXPIRED ) {
+                    whereAmI.setValue("in delay task");
+                    golowerMech2();
+
+                }
+            }
+        });
+
+    }
+
     private void golowerMech2() {
-        this.addTask(new DeadReckonTask(this, lowerMech, liftDriveTrain) {
+        this.addTask(new DeadReckonTask(this, lowerMech2, liftDriveTrain) {
             @Override
             public void handleEvent(RobotEvent e) {
                 DeadReckonEvent path = (DeadReckonEvent) e;
                 if (path.kind == EventKind.PATH_DONE) {
                     whereAmI.setValue("lifted linear lift");
+                    grabcone();
 
 
                 }
