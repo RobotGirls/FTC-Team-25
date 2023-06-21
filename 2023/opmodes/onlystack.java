66a414e4a652ad9024f9ead20f699c77ba2ade4c
hAudreyLim <audreylim@rocknrollrobots25.com>
2023-06-21 12:40:03 -0700

    Created an autonomous class template

---
+++ 2023/opmodes/onlystack.java
@@ -0,0 +1,348 @@
+/*
+Copyright (c) September 2017 FTC Teams 25/5218
+All rights reserved.
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+Neither the name of FTC Teams 25/5218 nor the names of their contributors may be used to
+endorse or promote products derived from this software without specific prior
+written permission.
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package opmodes;
+
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
+import com.qualcomm.hardware.rev.RevColorSensorV3;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.RobotLog;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.openftc.apriltag.AprilTagDetection;
+
+import team25core.DeadReckonPath;
+import team25core.DeadReckonTask;
+import team25core.DistanceSensorCriteria;
+import team25core.FourWheelDirectDrivetrain;
+import team25core.OneWheelDirectDrivetrain;
+import team25core.Robot;
+import team25core.RobotEvent;
+import team25core.vision.apriltags.AprilTagDetectionTask;
+
+
+@Autonomous(name = "ONLYSTACK")
+//@Disabled
+public class onlystack extends Robot {
+
+
+    //wheels
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+    private FourWheelDirectDrivetrain drivetrain;
+
+    static final double TURRET_TURN90 = 5;
+
+    //mechs
+    private Servo umbrella;
+
+    private DcMotor linearLift;
+    private OneWheelDirectDrivetrain liftDriveTrain;
+
+    private DcMotor turret;
+    private OneWheelDirectDrivetrain turretDrivetrain;
+
+
+    //sensors
+    private DistanceSensor alignerDistanceSensor;
+    private DistanceSensorCriteria distanceSensorCriteria;
+    private ColorSensor linearColorSensor;
+
+
+    //paths
+    private DeadReckonPath goStraightPath;
+    private DeadReckonPath goBackPath;
+    private DeadReckonPath path1;
+
+
+
+    private DeadReckonPath liftMech;
+    private DeadReckonPath  lowerMech;
+
+    private DeadReckonPath turretTurn90CW;
+    private DeadReckonPath turretTurn90CCW;
+
+
+    private DeadReckonPath deliverConePath;
+
+    //variables for constants
+
+    private int condition;
+
+
+
+
+    //telemetry
+    private Telemetry.Item whereAmI;
+
+    /*
+     * The default event handler for the robot.
+     */
+
+    @Override
+    public void handleEvent(RobotEvent e)
+    {
+        /*
+         * Every time we complete a segment drop a note in the robot log.
+         */
+        if (e instanceof DeadReckonTask.DeadReckonEvent) {
+            RobotLog.i("Completed path segment %d", ((DeadReckonTask.DeadReckonEvent)e).segment_num);
+        }
+    }
+
+
+
+
+
+    public void initPaths()
+    {
+        goStraightPath = new DeadReckonPath();
+        goStraightPath.addSegment(DeadReckonPath.SegmentType.STRAIGHT, 34, -0.5);
+        goStraightPath.stop();
+
+        path1 = new DeadReckonPath();
+        path1.addSegment(DeadReckonPath.SegmentType.SIDEWAYS, 45, 0.5);
+        path1.addSegment(DeadReckonPath.SegmentType.STRAIGHT, 10, -0.5);
+        path1.addSegment(DeadReckonPath.SegmentType.STRAIGHT, 10, 0.5);
+        path1.stop();
+
+
+        goBackPath = new DeadReckonPath();
+        goBackPath.addSegment(DeadReckonPath.SegmentType.STRAIGHT, 38, 0.5);
+        goBackPath.stop();
+
+
+        liftMech = new DeadReckonPath();
+        liftMech.addSegment(DeadReckonPath.SegmentType.STRAIGHT, 68, -0.5);
+
+        lowerMech =  new DeadReckonPath();
+        lowerMech.addSegment(DeadReckonPath.SegmentType.STRAIGHT, 1.5, -0.01);
+
+        turretTurn90CW.addSegment(DeadReckonPath.SegmentType.STRAIGHT,TURRET_TURN90, 0.5);
+        turretTurn90CCW.addSegment(DeadReckonPath.SegmentType.STRAIGHT,TURRET_TURN90, -0.5);
+
+
+
+
+
+    }
+
+    @Override
+    public void init()
+    {
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+        umbrella=hardwareMap.servo.get("umbrella");
+
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        drivetrain = new FourWheelDirectDrivetrain(frontRight, backRight, frontLeft, backLeft);
+        drivetrain.resetEncoders();
+        drivetrain.encodersOn();
+
+        whereAmI = telemetry.addData("location in code", "init");
+
+
+        linearLift=hardwareMap.get(DcMotor.class, "linearLift");
+        linearLift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        liftDriveTrain = new OneWheelDirectDrivetrain(linearLift);
+        liftDriveTrain.resetEncoders();
+        liftDriveTrain.encodersOn();
+
+        turret = hardwareMap.get(DcMotor.class, "turret");
+        turret.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        turretDrivetrain = new OneWheelDirectDrivetrain(turret);
+        turretDrivetrain.resetEncoders();
+        turretDrivetrain.encodersOn();
+
+        //open umbrella & lock cone
+        umbrella.setPosition(0.5);
+
+
+        linearColorSensor = hardwareMap.get(RevColorSensorV3.class, "liftColorSensor");
+        alignerDistanceSensor = hardwareMap.get(Rev2mDistanceSensor.class, "alignerDistanceSensor");
+
+
+        initPaths();
+
+
+    }
+
+
+
+
+    //  lifting & dropping paths --------------------------------------
+
+    private void lockCone() {
+        umbrella.setPosition(0.5);
+        goliftMech();
+
+    }
+
+    private void goliftMech() {
+        this.addTask(new DeadReckonTask(this, liftMech, liftDriveTrain) {
+            @Override
+            public void handleEvent(RobotEvent e) {
+                DeadReckonEvent path = (DeadReckonEvent) e;
+                if (path.kind == EventKind.PATH_DONE) {
+                    whereAmI.setValue("lifted linear lift");
+                    condition = 2;
+                    //goDrive(goBackPath);
+
+
+
+                }
+            }
+        });
+    }
+
+    private void goTurnTurret90(DeadReckonPath turnpath) {
+        this.addTask(new DeadReckonTask(this, turnpath, turretDrivetrain) {
+            @Override
+            public void handleEvent(RobotEvent e) {
+                DeadReckonEvent path = (DeadReckonEvent) e;
+                if (path.kind == EventKind.PATH_DONE) {
+                    whereAmI.setValue("turned turret");
+                    dropCone();
+
+
+
+                }
+            }
+        });
+    }
+
+    private void dropCone() {
+        umbrella.setPosition(0);
+        goTurnTurret90(turretTurn90CCW);
+
+    }
+
+
+    private void golowerMech() {
+        this.addTask(new DeadReckonTask(this, lowerMech, liftDriveTrain) {
+            @Override
+            public void handleEvent(RobotEvent e) {
+                DeadReckonEvent path = (DeadReckonEvent) e;
+                if (path.kind == EventKind.PATH_DONE) {
+                    whereAmI.setValue("lifted linear lift");
+                    lockCone();
+
+
+
+
+
+
+
+                }
+            }
+        });
+    }
+
+
+
+    // parking paths -----------------------------------
+
+
+//    public void goDrive(DeadReckonPath detectedZonePark)
+//    {
+//
+//
+//
+//        this.addTask(new DeadReckonTask(this, detectedZonePark ,drivetrain ){
+//            @Override
+//            public void handleEvent(RobotEvent e) {
+//                DeadReckonEvent path = (DeadReckonEvent) e;
+//                if (path.kind == EventKind.PATH_DONE)
+//                {
+//                    whereAmI.setValue("went to park");
+//                    if ( distanceSensorCriteria.equals(3.6) )
+//                    {
+//                        golowerMech();
+//                    }
+//                    if (  condition == 2)
+//                    {
+//                        goTurnTurret90(turretTurn90CW);
+//                    }
+//
+//
+//                }
+//            }
+//        });
+//    }
+
+    public void goDrive()
+    {
+
+
+
+        this.addTask(new DeadReckonTask(this, path1 ,drivetrain ){
+            @Override
+            public void handleEvent(RobotEvent e) {
+                DeadReckonEvent path = (DeadReckonEvent) e;
+                if (path.kind == EventKind.PATH_DONE)
+                {
+
+
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public void start()
+    {
+//        whereAmI.setValue("in Start");
+//
+//        int value = 5;
+//
+//        while ( value > 0 )
+//        {
+//            goDrive(goStraightPath);
+//            value--;
+//        }
+         goDrive();
+
+
+
+    }
+}
---
+++ 2023/opmodes/pathsforauto.java
@@ -0,0 +1,243 @@
+/*
+Copyright (c) September 2017 FTC Teams 25/5218
+All rights reserved.
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+Neither the name of FTC Teams 25/5218 nor the names of their contributors may be used to
+endorse or promote products derived from this software without specific prior
+written permission.
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package opmodes;
+
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
+import com.qualcomm.hardware.rev.RevColorSensorV3;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.RobotLog;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.openftc.apriltag.AprilTagDetection;
+
+import team25core.DeadReckonPath;
+import team25core.DeadReckonTask;
+import team25core.DistanceSensorCriteria;
+import team25core.FourWheelDirectDrivetrain;
+import team25core.OneWheelDirectDrivetrain;
+import team25core.Robot;
+import team25core.RobotEvent;
+import team25core.RunToEncoderValueTask;
+import team25core.vision.apriltags.AprilTagDetectionTask;
+
+
+@Autonomous(name = "TESTauto")
+//@Disabled
+public class pathsforauto extends Robot {
+
+
+    //wheels
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+    private FourWheelDirectDrivetrain drivetrain;
+
+
+    //mechs
+    private Servo umbrella;
+
+    private DcMotor linearLift;
+    private OneWheelDirectDrivetrain liftDriveTrain;
+
+    private DcMotor turret;
+    private OneWheelDirectDrivetrain turretDrivetrain;
+
+
+    //sensors
+    private DistanceSensor alignerDistanceSensor;
+    private DistanceSensorCriteria distanceSensorCriteria;
+    private ColorSensor linearColorSensor;
+
+
+    //paths
+    private DeadReckonPath leftPath;
+    private DeadReckonPath middlePath;
+    private DeadReckonPath rightPath;
+
+
+    private DeadReckonPath liftMech;
+    private DeadReckonPath  lowerMech;
+
+    private DeadReckonPath turretTurnOrangePath;
+    private DeadReckonPath turretTurnBluePath;
+
+    private DeadReckonPath randompath;
+
+    private DeadReckonPath deliverConePath;
+
+    //variables for constants
+    static final double FORWARD_DISTANCE = 13.5;
+    static final double DRIVE_SPEED = 0.25;
+
+    // apriltags detection
+    private Telemetry.Item tagIdTlm;
+    private Telemetry.Item parkingLocationTlm;
+    AprilTagDetection tagObject;
+    private AprilTagDetectionTask detectionTask;
+
+    //telemetry
+    private Telemetry.Item whereAmI;
+
+    private RunToEncoderValueTask linearLiftTask;
+
+    /*
+     * The default event handler for the robot.
+     */
+
+    @Override
+    public void handleEvent(RobotEvent e)
+    {
+        /*
+         * Every time we complete a segment drop a note in the robot log.
+         */
+        if (e instanceof DeadReckonTask.DeadReckonEvent) {
+            RobotLog.i("Completed path segment %d", ((DeadReckonTask.DeadReckonEvent)e).segment_num);
+        }
+    }
+
+
+
+
+
+    public void initPaths()
+    {
+
+        middlePath = new DeadReckonPath();
+
+        middlePath.addSegment(DeadReckonPath.SegmentType.SIDEWAYS, FORWARD_DISTANCE+6, -DRIVE_SPEED);
+        middlePath.addSegment(DeadReckonPath.SegmentType.STRAIGHT, FORWARD_DISTANCE+6, -DRIVE_SPEED);
+        middlePath.addSegment(DeadReckonPath.SegmentType.STRAIGHT, FORWARD_DISTANCE+6, DRIVE_SPEED);
+
+        //og37
+        middlePath.stop();
+
+
+
+    }
+
+    @Override
+    public void init()
+    {
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+        umbrella=hardwareMap.servo.get("umbrella");
+
+
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        drivetrain = new FourWheelDirectDrivetrain(frontRight, backRight, frontLeft, backLeft);
+        drivetrain.resetEncoders();
+        drivetrain.encodersOn();
+
+        whereAmI = telemetry.addData("location in code", "init");
+        tagIdTlm = telemetry.addData("tagId","none");
+        parkingLocationTlm = telemetry.addData("parking location: ","none");
+
+        linearLift=hardwareMap.get(DcMotor.class, "linearLift");
+        linearLift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        liftDriveTrain = new OneWheelDirectDrivetrain(linearLift);
+        liftDriveTrain.resetEncoders();
+        liftDriveTrain.encodersOn();
+
+        turret = hardwareMap.get(DcMotor.class, "turret");
+        turret.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        turret.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        turret.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        turretDrivetrain = new OneWheelDirectDrivetrain(turret);
+        turretDrivetrain.resetEncoders();
+        turretDrivetrain.encodersOn();
+
+        //open umbrella & lock cone
+        umbrella.setPosition(0.55);
+
+
+        linearColorSensor = hardwareMap.get(RevColorSensorV3.class, "liftColorSensor");
+        alignerDistanceSensor = hardwareMap.get(Rev2mDistanceSensor.class, "alignerDistanceSensor");
+
+        turret.setTargetPosition(0);
+        turret.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        turret.setPower(0.5);
+
+
+        initPaths();
+
+
+    }
+
+
+
+
+    //  lifting & dropping paths --------------------------------------
+
+
+
+
+
+    public void gotoMiddlePark()
+    {
+        parkingLocationTlm.setValue("went to middle target zone");
+
+        this.addTask(new DeadReckonTask(this, middlePath,drivetrain ){
+            @Override
+            public void handleEvent(RobotEvent e) {
+                DeadReckonEvent path = (DeadReckonEvent) e;
+                if (path.kind == EventKind.PATH_DONE)
+                {
+                    RobotLog.i("went to middle target zone");
+                    whereAmI.setValue("went to middle target zone");
+                    //goTurnTurret();
+
+
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public void start()
+    {
+        whereAmI.setValue("in Start");
+        gotoMiddlePark();
+
+
+    }
+}
