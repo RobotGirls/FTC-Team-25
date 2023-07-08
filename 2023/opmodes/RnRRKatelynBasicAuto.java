048b58ee92ec07e236d6f4ea994f933558c43110
katelynl19 <katelynliu419@gmail.com>
2023-07-08 11:06:42 -0700

    added strafe and turn paths

--- 2023/opmodes/RnRRKatelynBasicAuto.java
+++ 2023/opmodes/RnRRKatelynBasicAuto.java
@@ -7,24 +7,35 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.RobotLog;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 import team25core.DeadReckonPath;
 import team25core.DeadReckonTask;
+import team25core.FourWheelDirectDrivetrain;
 import team25core.KatelynsFourWheelDrivetrain;
 import team25core.RobotEvent;
 import team25core.StandardFourMotorRobot;
 
-@Autonomous(name = "Katelyn's Auto",group = "rnrr")
+@Autonomous(name = "KatelynAuto",group = "rnrr")
 public class RnRRKatelynBasicAuto extends StandardFourMotorRobot {
 
 
-    private KatelynsFourWheelDrivetrain drivetrain;
+    //private KatelynsFourWheelDrivetrain drivetrain;
+    private FourWheelDirectDrivetrain drivetrain;
     private DeadReckonPath straightPath;
+
+    private DeadReckonPath strafePath;
+
+    private DeadReckonPath turnPath;
+
+    private Telemetry.Item eventTlm;
 
     @Override
     public void init() {
         super.init(); // taking parent's init (hardware mapping)
         //instantiating the KatelynsFourWheelDrivetrain class
-        drivetrain = new KatelynsFourWheelDrivetrain(frontRight, backRight, frontLeft, backLeft);
+        //drivetrain = new KatelynsFourWheelDrivetrain(frontRight, backRight, frontLeft, backLeft);
+        drivetrain = new FourWheelDirectDrivetrain(frontRight, backRight, frontLeft, backLeft);
         // uncomment the following only if the robot is going in the opposite direction from intended
         //drivetrain.setCanonicalMotorDirection();
 
@@ -32,37 +43,90 @@ public class RnRRKatelynBasicAuto extends StandardFourMotorRobot {
         drivetrain.encodersOn();
 
         // Sets the behavior of the motor when a power level of zero is applied i.e. not moving - when we apply 0 power, the motor brakes
-        drivetrain.brakeOnZeroPower();
+        //drivetrain.brakeOnZeroPower();
 
         // Sets motor encoder position to 0
         drivetrain.resetEncoders();
+
+        eventTlm = telemetry.addData("pathEvent", "none");
 
         initPaths();
     }
 
     public void initPaths() {
-        DeadReckonPath straightPath = new DeadReckonPath();
+        straightPath = new DeadReckonPath();
+        strafePath = new DeadReckonPath();
+        turnPath = new DeadReckonPath();
+
         straightPath.stop();
-        straightPath.addSegment(DeadReckonPath.SegmentType.STRAIGHT,1.2, STRAIGHT_SPEED);
+        strafePath.stop();
+        turnPath.stop();
+
+        straightPath.addSegment(DeadReckonPath.SegmentType.STRAIGHT,1.2, 0.3);
+        strafePath.addSegment(DeadReckonPath.SegmentType.SIDEWAYS, 2, 0.3);
+        turnPath.addSegment(DeadReckonPath.SegmentType.TURN, 1.5, 0.3);
     }
 
     public void goForward() {
         this.addTask(new DeadReckonTask(this, straightPath, drivetrain){
             @Override
             public void handleEvent (RobotEvent e){
-                // could use a little clarification on this - what is the DeadReckonEvent line doing?
-                DeadReckonEvent path = (DeadReckonEvent) e;
-                if (path.kind == EventKind.PATH_DONE)
+                DeadReckonEvent pathEvent = (DeadReckonEvent) e;
+                if (pathEvent.kind == EventKind.PATH_DONE)
                 {
                     RobotLog.i("went forward");
+                    eventTlm.setValue("path is done");
+                    strafe();
                 }
-
+                else if (pathEvent.kind == EventKind.SEGMENT_DONE) {
+                    RobotLog.i("segment completed");
+                    eventTlm.setValue("segment is done");
+                }
             }
         });
     }
 
+    public void strafe() {
+        this.addTask(new DeadReckonTask(this, strafePath, drivetrain){
+            @Override
+            public void handleEvent (RobotEvent e) {
+                DeadReckonEvent pathEvent = (DeadReckonEvent) e;
+                if (pathEvent.kind == EventKind.PATH_DONE) {
+                    RobotLog.i("strafed");
+                    eventTlm.setValue("path is done");
+                    turnRight();
+                }
+                else if (pathEvent.kind == EventKind.SEGMENT_DONE) {
+                    RobotLog.i("segment completed");
+                    eventTlm.setValue("segment is done");
+                }
+            }
+        });
+    }
+
+    public void turnRight() {
+        this.addTask(new DeadReckonTask(this, turnPath, drivetrain) {
+            @Override
+            public void handleEvent (RobotEvent e) {
+                DeadReckonEvent pathEvent = (DeadReckonEvent) e;
+                if (pathEvent.kind == EventKind.PATH_DONE) {
+                    RobotLog.i("turned");
+                    eventTlm.setValue("path is done");
+                }
+                else if (pathEvent.kind == EventKind.SEGMENT_DONE) {
+                    RobotLog.i("segment completed");
+                    eventTlm.setValue("segment is done");
+                }
+            }
+        });
+    }
+
+
+    @Override
     public void start() {
-        // what do I need to add to this method?
+        goForward();
+        //strafe();
+        //turnRight();
     }
 
 }
