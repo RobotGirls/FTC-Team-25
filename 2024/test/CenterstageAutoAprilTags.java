4ad84081580d550ddb0ef48e9dd614b6de87dce6
katelynl19 <katelynliu@rocknrollrobots25.com>
2023-11-06 20:18:54 -0800

    added method to center on AprilTag

--- 2024/test/CenterstageAutoAprilTags.java
+++ 2024/test/CenterstageAutoAprilTags.java
@@ -2,6 +2,7 @@ package test;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.util.RobotLog;
 
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
@@ -16,25 +17,25 @@ import team25core.RobotEvent;
 public class CenterstageAutoAprilTags extends Robot {
     private ObjectDetectionNewTask objDetectionTask;
     private final static String TAG = "Prop";
-    final double DESIRED_DISTANCE = 12.0; //  this is how close the camera should get to the target (inches)
+    final double DESIRED_DISTANCE = 6.0; //  this is how close the camera should get to the target (inches)
     //  Set the GAIN constants to control the relationship between the measured position error,
     //  and how much power is applied to the drive motors to correct the error.
     //  Drive = Error * Gain    Make these values smaller for smoother control, or larger
     //  for a more aggressive response.
-    final double SPEED_GAIN  =  0.02  ;   //  Forward Speed Control "Gain". eg: Ramp up to 50% power at a 25 inch error.   (0.50 / 25.0)
-    final double STRAFE_GAIN =  0.015 ;   //  Strafe Speed Control "Gain".  eg: Ramp up to 25% power at a 25 degree Yaw error.   (0.25 / 25.0)
-    final double TURN_GAIN   =  0.01  ;   //  Turn Control "Gain".  eg: Ramp up to 25% power at a 25 degree error. (0.25 / 25.0)
+    final double SPEED_GAIN = 0.02;   //  Forward Speed Control "Gain". eg: Ramp up to 50% power at a 25 inch error.   (0.50 / 25.0)
+    final double STRAFE_GAIN = 0.015;   //  Strafe Speed Control "Gain".  eg: Ramp up to 25% power at a 25 degree Yaw error.   (0.25 / 25.0)
+    final double TURN_GAIN = 0.01;   //  Turn Control "Gain".  eg: Ramp up to 25% power at a 25 degree error. (0.25 / 25.0)
 
     final double MAX_AUTO_SPEED = 0.5;   //  Clip the approach speed to this max value (adjust for your robot)
-    final double MAX_AUTO_STRAFE= 0.5;   //  Clip the approach speed to this max value (adjust for your robot)
-    final double MAX_AUTO_TURN  = 0.3;   //  Clip the turn speed to this max value (adjust for your robot)
+    final double MAX_AUTO_STRAFE = 0.5;   //  Clip the approach speed to this max value (adjust for your robot)
+    final double MAX_AUTO_TURN = 0.3;   //  Clip the turn speed to this max value (adjust for your robot)
 
     private DcMotor frontLeft;
     private DcMotor frontRight;
     private DcMotor backLeft;
     private DcMotor backRight;
     private FourWheelDirectDrivetrain drivetrain;
-    
+
     private int desiredTagID;     // Choose the tag you want to approach or set to -1 for ANY tag.
 
     private final float APRIL_TAG_DECIMATION = 2;
@@ -44,21 +45,21 @@ public class CenterstageAutoAprilTags extends Robot {
 
     public String position = "left"; // this will contain the actual prop position information in final auto
 
+    public AprilTagDetection aprilTag;
+
     @Override
-    public void handleEvent(RobotEvent e)
-    {
+    public void handleEvent(RobotEvent e) {
         /*
          * Every time we complete a segment drop a note in the robot log.
          */
         //if (e instanceof DeadReckonTask.DeadReckonEvent) {
-          //  RobotLog.i("Completed path segment %d", ((DeadReckonTask.DeadReckonEvent)e).segment_num);
+        //  RobotLog.i("Completed path segment %d", ((DeadReckonTask.DeadReckonEvent)e).segment_num);
         //}
     }
 
-    public void findAprilTag()
-    {
+    public void findAprilTag() {
         RobotLog.ii(TAG, "Setup findAprilTag");
-        objDetectionTask = new ObjectDetectionNewTask(this, telemetry, ObjectDetectionNewTask.DetectionKind.APRILTAG_DETECTED){
+        objDetectionTask = new ObjectDetectionNewTask(this, telemetry, ObjectDetectionNewTask.DetectionKind.APRILTAG_DETECTED) {
             @Override
             public void handleEvent(RobotEvent e) {
                 TagDetectionEvent event = (TagDetectionEvent) e;
@@ -84,34 +85,31 @@ public class CenterstageAutoAprilTags extends Robot {
     public void findDesiredID() {
         if (position.equals("left")) {
             desiredTagID = 1; // 4 on red
-        }
-        else if (position.equals("center")) {
+        } else if (position.equals("center")) {
             desiredTagID = 2; // 5 on red
-        }
-        else {
+        } else {
             desiredTagID = 3; // 6 on red
         }
-        findAprilTagData();
+        //findAprilTagData();
     }
 
     public AprilTagDetection findAprilTagData() {
         if (desiredTagID == 1) {
             while (objDetectionTask.getAprilTag(desiredTagID) == null) {
-                frontLeft.setPower(-0.3);
+                frontLeft.setPower(0.3);
                 frontRight.setPower(0.3);
-                backLeft.setPower(0.3);
+                backLeft.setPower(-0.3);
                 backRight.setPower(-0.3);
             }
             frontLeft.setPower(0);
             frontRight.setPower(0);
             backLeft.setPower(0);
             backRight.setPower(0);
-        }
-        else if (desiredTagID == 2) {
+        } else if (desiredTagID == 3) {
             while (objDetectionTask.getAprilTag(desiredTagID) == null) {
-                frontLeft.setPower(0.3);
+                frontLeft.setPower(-0.3);
                 frontRight.setPower(-0.3);
-                backLeft.setPower(-0.3);
+                backLeft.setPower(0.3);
                 backRight.setPower(0.3);
             }
             frontLeft.setPower(0);
@@ -122,6 +120,50 @@ public class CenterstageAutoAprilTags extends Robot {
         return objDetectionTask.getAprilTag(desiredTagID);
     }
 
+    public void AlignWithAprilTag(AprilTagDetection tag) {
+       double drive = 0;
+       double strafe = 0;
+       double turn = 0;
+        if (objDetectionTask.getAprilTag(desiredTagID) != null) {
+
+            double rangeError = (tag.ftcPose.range - DESIRED_DISTANCE);
+            double headingError = tag.ftcPose.bearing;
+            double yawError = tag.ftcPose.yaw;
+
+            drive = Range.clip(rangeError * SPEED_GAIN, -MAX_AUTO_SPEED, MAX_AUTO_SPEED);
+            turn = Range.clip(headingError * TURN_GAIN, -MAX_AUTO_TURN, MAX_AUTO_TURN);
+            strafe = Range.clip(-yawError * STRAFE_GAIN, -MAX_AUTO_STRAFE, MAX_AUTO_STRAFE);
+        }
+        telemetry.update();
+
+        // Apply desired axes motions to the drivetrain.
+        moveRobot(drive, strafe, turn);
+    }
+    public void moveRobot(double x, double y, double yaw) {
+        // Calculate wheel powers.
+        double leftFrontPower    =  x -y +yaw;
+        double rightFrontPower   =  x +y -yaw;
+        double leftBackPower     =  x +y +yaw;
+        double rightBackPower    =  x -y -yaw;
+
+        // Normalize wheel powers to be less than 1.0
+        double max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+        max = Math.max(max, Math.abs(leftBackPower));
+        max = Math.max(max, Math.abs(rightBackPower));
+
+        if (max > 1.0) {
+            leftFrontPower /= max;
+            rightFrontPower /= max;
+            leftBackPower /= max;
+            rightBackPower /= max;
+        }
+
+        // Send powers to the wheels.
+        frontLeft.setPower(leftFrontPower);
+        frontRight.setPower(rightFrontPower);
+        backLeft.setPower(leftBackPower);
+        backRight.setPower(rightBackPower);
+    }
 
 
 
@@ -132,11 +174,14 @@ public class CenterstageAutoAprilTags extends Robot {
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
         backLeft = hardwareMap.get(DcMotor.class, "backLeft");
         backRight = hardwareMap.get(DcMotor.class, "backRight");
-
     }
    @Override
    public void start(){
         findDesiredID();
+       //desiredTagID = 3;
         findAprilTag();
+        aprilTag = findAprilTagData();
+        AlignWithAprilTag(aprilTag);
+
    }
 }
