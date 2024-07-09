96846d3cf68b72044f10ad13c24948ea9c679843
katelynl19 <katelynliu@rocknrollrobots25.com>
2024-07-08 21:08:59 -0700

    added hang, intake, and servos into the rr teleop

--- 2024/opmodes/RRTeleop.java
+++ 2024/opmodes/RRTeleop.java
@@ -23,9 +23,29 @@ public class RRTeleop extends LinearOpMode {
         CenterstageSampleMecanumDrive drive = new CenterstageSampleMecanumDrive(hardwareMap);
         drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
+        DcMotor rightHang;
+        DcMotor leftHang;
+        DcMotor linearLift;
+
+        boolean intakeOn = false;
+        boolean outtakeOn = false;
+
+        rightHang = hardwareMap.get(DcMotor.class, "rightHang");
+        rightHang.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightHang.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightHang.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        leftHang = hardwareMap.get(DcMotor.class, "leftHang");
+        leftHang.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftHang.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftHang.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        linearLift = hardwareMap.get(DcMotor.class, "linearLift");
+        linearLift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        linearLift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        linearLift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
         Pose2d startPose = new Pose2d(0, 0, Math.toRadians(0));
-
-
 
         drive.setPoseEstimate(startPose);
 
@@ -61,6 +81,71 @@ public class RRTeleop extends LinearOpMode {
                 // pixel box is open
                 drive.pixelRelease.setPosition(BLOCK_NOTHING);
             }
+            else if (gamepad2.y) {
+                leftHang.setPower(1);
+                rightHang.setPower(-1);
+            }
+            else if (gamepad2.a) {
+                leftHang.setPower(-1);
+                rightHang.setPower(1);
+            }
+            else if (gamepad2.x) {
+                leftHang.setPower(1);
+            }
+            else if (gamepad2.b) {
+                leftHang.setPower(-1);
+            }
+            else if (!gamepad2.y && !gamepad2.a) {
+                rightHang.setPower(0);
+                leftHang.setPower(0);
+            }
+            else if (!gamepad2.y && !gamepad2.a && !gamepad2.x && !gamepad2.b) {
+                leftHang.setPower(0);
+                rightHang.setPower(0);
+            }
+            else if (gamepad2.right_bumper) {
+                drive.linkage.setPosition(drive.LINKAGE_UP);
+            }
+            else if (gamepad2.left_bumper) {
+                drive.linkage.setPosition(drive.LINKAGE_DOWN);
+            }
+
+            if (gamepad1.right_bumper) {
+                if(intakeOn == false) {
+                    drive.intake.setPower(-0.9);
+                    intakeOn = true;
+                }
+                else {
+                    drive.intake.setPower(0);
+                    intakeOn = false;
+                }
+            }
+            else if (gamepad1.left_bumper) {
+                //outtake pixels
+                if(outtakeOn == false) {
+                    drive.intake.setPower(0.8);
+                    outtakeOn = true;
+                }
+                else {
+                    drive.intake.setPower(0);
+                    outtakeOn = false;
+                }
+
+                drive.box.setPosition(0.465);
+                drive.pixelRelease.setPosition(BLOCK_BOTH);
+            }
+            else if (gamepad1.dpad_down) {
+                // flip box to original position block pixels from falling
+                drive.pixelRelease.setPosition(BLOCK_BOTH);
+                drive.box.setPosition(0.89);
+            }
+            else if (gamepad1.dpad_up) {
+                // box up to score and block pixels
+                drive.box.setPosition(0.465);
+                drive.pixelRelease.setPosition(BLOCK_BOTH);
+            }
+
+
             // Update everything. Odometry. Etc.
             drive.update();
 
